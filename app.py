acf7a19bd7994b7924acfa89fd55d2942e6fd3eb
Willemaarts <willemaarts@outlook.com>
2022-12-01 16:29:29 +0100

    V0.1 (Started process of converting)

--- app.py
+++ app.py
@@ -13,17 +13,92 @@ import openpyxl
 # Set up page config
 st.set_page_config(page_title="Forecast converter - JUYO", page_icon=":arrows_clockwise:", layout="wide", initial_sidebar_state="collapsed")
 
-# Hide streamlit footer
+# Hide streamlit footer; (header {visibility hidden;})
 hide_default_format = """
        <style>
        footer {visibility: hidden;}
-       header {visibility: hidden;}
        </style>
        """
 st.markdown(hide_default_format, unsafe_allow_html=True)
 
 def run_process():
-    print(f"starting process...")
+    with st.spinner('runnning process...'):
+
+        print(f"starting process...")
+        
+        iSegments = []
+        iMonths = []
+        iTerm = []
+        iDays = []
+        iSort = []
+        iData = []
+        sMonths = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Sept","Oct","Nov","Dec"]
+
+        for x in sorted_items2: iSegments.append(x)
+        for x in cols: iMonths.append(x)
+        for x in terminology: iTerm.append(x)
+        for x in iMonths:
+            str2Match = x
+            strOptions = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Sept","Oct","Nov","Dec"]
+
+            highest = process.extractOne(str2Match,strOptions)
+            highest = highest
+            iDays.append(highest)
+        
+        for x in keywords: iSort.append(x)
+
+        print(f"segments: {iSegments}")
+        print(f"sheets: {iMonths}")
+        print(f"terminology: {iTerm}")
+        print(f"months: {iDays[:3]}") # Use if in to see if month contains. https://stackabuse.com/python-check-if-string-contains-substring/
+        print(f"sorting: {iSort}")
+
+        for x in range(len(iMonths)):
+
+            wb = openpyxl.load_workbook(uploaded_file_CLIENT, data_only=True)
+
+            ws = wb[(iMonths[x])]
+
+            for z in sMonths:
+                try:
+                    iDays[x].index(z)
+                except ValueError:
+                    pass
+                else:
+                    tMonth = z
+
+            print(f'Current month: {tMonth}')
+            if tMonth == 'Jan' or tMonth == 'Mar' or tMonth == 'May' or tMonth == 'Jul' or tMonth == 'Aug' or tMonth == 'Oct' or tMonth == 'Dec': mDay = 31
+            elif tMonth == 'Apr' or tMonth == 'Jun' or tMonth == 'Sep' or tMonth == 'Sept' or tMonth == 'Nov': mDay = 30
+            elif tMonth == 'Feb': mDay = 28
+            else: mDay = 30
+            
+            if storage == 'Rows':
+
+                for i in range(1, ws.max_row + 1):
+                    for j in range(1, ws.max_column + 1):
+                        if i == int(row_n):
+                            if "Rms" == ws.cell(i,j).value:
+                                #print(f"found {ws.cell(i,j)}")
+
+                                for row in ws.iter_rows(min_row=i + 1,max_row=i + mDay, min_col=j, max_col=j):
+                                    for cell in row:
+                                        iData.append(cell.value)# = [cell.value]
+                                        #print(cell.value)
+                                iData.append("Rms")
+            else:
+                
+                for i in range(1, ws.max_row + 1):
+                    for j in range(1, ws.max_column + 1):
+                        if j == int(row_n):
+                            if "Rms" == ws.cell(i,j).value:
+                                print(f"found {ws.cell(i,j)}")
+
+
+            iData.append(iMonths[x])
+            print(iData)
+    st.success('Done!')
+    
 
 # Header of the page
 with st.container():
@@ -66,6 +141,8 @@ with st.container():
             tabs = pd.ExcelFile(uploaded_file_CLIENT).sheet_names
 
             cols = st.multiselect('select sheets:', tabs)
+
+            st.write(cols)
             
             try:
 
@@ -76,20 +153,11 @@ with st.container():
                 strOptions = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Sept","Oct","Nov","Dec"]
                 
                 Ratios = process.extract(str2Match,strOptions)
-                print(Ratios)
 
                 highest = process.extractOne(str2Match,strOptions)
-                print(highest)
 
                 st.write(Ratios)
                 st.write(highest)
-
-                for col in cols:
-                    str2Match = cols[col]
-                    strOptions = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Sept","Oct","Nov","Dec"]
-                    
-                    highest = process.extractOne(str2Match,strOptions)
-                    print(highest)
             
             except:
                 with st.empty():
@@ -154,7 +222,7 @@ try:
                 sorted_items1 = sort_items(df1.columns.to_list(), direction='vertical')
 
             with right_column:
-                st.markdown("Reorder the segments is right order.")
+                st.markdown("map the segments is right order.")
                 sorted_items2 = sort_items(keywords, direction='vertical')
 
         year1 = datetime.today().year
@@ -167,7 +235,7 @@ try:
 
         if year:
             terminology = st_tags(
-            label='# Enter the terminology used in Excel file (e.g.: Rn & REV):',
+            label='# Enter the terminology used in Excel file (e.g.: Rms & ZAR):',
             text='Press enter to add more',
             suggestions=['rn', 'RN', 'Rn', 
                         'Rev', 'REV', 'rev', 
@@ -175,21 +243,34 @@ try:
                         ],
             maxtags = 2,
             key='2')
-        
+
+            st.warning('  Always first the terminology of RoomNights, then REV or ADR!!!', icon="⚠️")
             st.write(len(terminology), ' terminology of ', 2, ' entered.')
             st.write(terminology)
 
         if len(terminology) == 2:
-            calculate = st.radio(
+            option = st.radio(
                 "is the data stored as Rn & Rev or Rn & ADR?",
-                ('Rn & Rev', 'Rn & ADR', 'No disered option'))
+                ('No disered option', 'Rn & Rev', 'Rn & ADR'))
 
-            if calculate == 'Rn & Rev':
+            if option == 'Rn & Rev':
                 st.write('You selected Rn & Rev.')
-            elif calculate == 'Rn & ADR':
+            elif option == 'Rn & ADR':
                 st.write('You selected Rn & ADR.')
             else:
                 st.write("You didn't select anything, if disered combination isn't present, please contact JUYO.")
+
+            storage = st.radio(
+                "is the data stored in rows or in columns?",
+                ('Rows', 'Columns'))
+
+            if storage == 'Rows':
+                row_n = st.text_input("in which row can the terminology be found?")
+                #row_n = row_n + 1
+            else:
+                row_n = st.text_input("in which column can the terminology be found?")
+                row_n = ord(row_n) - 96
+                print(row_n)
 
 except:
     print('waiting...')
